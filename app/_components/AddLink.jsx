b482f0848efac23fcfa0f2d7d31b6bc0e470414d
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-09-10 22:38:13 +0200

    Added Functionality to change Platforms and add to Phone svg

--- app/_components/AddLink.jsx
+++ app/_components/AddLink.jsx
@@ -2,30 +2,14 @@
 import Image from "next/image";
 import React from "react";
 import linkIcon from "@/assets/images/icon-link.svg";
-import { useLinks } from "./useLinkContext";
-import { useEffect } from "react";
 
-export default function AddLink() {
-  const { url, setUrl } = useLinks();
-
-  function handleChange(e) {
+export default function AddLink({ id, link, onUpdateLink }) {
+  // Function to handle changes in both platform and link input fields
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    const newUrlData = { ...url };
-    newUrlData[name] = value;
-    setUrl(newUrlData);
-  }
-
-  // useEffect(() => {
-  //   const platform = document.querySelector("#platform");
-  //   const url = document.querySelector("#url");
-  //   const selectedValue = platform.value;
-
-  //   console.log(selectedValue);
-
-  //   setUrl((state) => ({ ...state, platform: selectedValue }));
-
-  //   setUrl((state) => ({ ...state, link: url.value }));
-  // }, []);
+    const updatedLink = { ...link, [name]: value };
+    onUpdateLink(id, updatedLink);
+  };
 
   return (
     <li className="p-5 mt-4 rounded-md bg-secondary">
@@ -39,14 +23,14 @@ export default function AddLink() {
           id="platform"
           className="p-2 rounded-md outline-none border-[1px] border-text-light"
           onChange={handleChange}
-          value={url.platform}
+          value={link.platform}
           name="platform"
-          defaultValue="github"
         >
-          <option>github</option>
-          <option>Google</option>
-          <option>Twitter</option>
-          <option>Leetcode</option>
+          <option value="">Select a platform</option>
+          <option value="Github">Github</option>
+          <option value="Google">Google</option>
+          <option value="Twitter">Twitter</option>
+          <option value="Leetcode">Leetcode</option>
         </select>
       </div>
       <div className="relative flex flex-col gap-1 mt-3">
@@ -58,7 +42,7 @@ export default function AddLink() {
           className="p-2 rounded-md outline-none border-[1px] border-text-light pl-10"
           placeholder="www.github.com/navi003"
           onChange={handleChange}
-          value={url.url}
+          value={link.link}
         />
         <Image
           src={linkIcon}
--- app/_components/UserLinksContainer.jsx
+++ app/_components/UserLinksContainer.jsx
@@ -7,13 +7,24 @@ import { useState } from "react";
 import { useLinks } from "./useLinkContext";
 
 export default function UserLinksContainer() {
-  const { url, setUrl, links, setLinks } = useLinks();
+  const { links, setLinks } = useLinks();
 
+  // Add an empty link object when the "Add new Link" button is clicked
   function addLinksHandler() {
-    setLinks((state) => [...state, url]);
+    setLinks((state) => [...state, { platform: "", link: "" }]);
   }
 
+  // Function to update individual links based on index
+  const updateLinkHandler = (index, updatedLink) => {
+    setLinks((prevLinks) => {
+      const newLinks = [...prevLinks];
+      newLinks[index] = updatedLink;
+      return newLinks;
+    });
+  };
+
   console.log(links);
+
   return (
     <div className="w-full bg-white rounded-lg p-14">
       <Heading text="Add/edit/remove links below and then share all your profiles with the world!">
@@ -23,15 +34,16 @@ export default function UserLinksContainer() {
         + Add new Link
       </Button>
       <ul>
-        {links.map((link, i) => {
-          return <AddLink key={i} />;
-        })}
+        {links.map((link, i) => (
+          <AddLink
+            key={i}
+            id={i}
+            link={link}
+            onUpdateLink={updateLinkHandler}
+          />
+        ))}
       </ul>
-      <Button
-        // onClick={}
-        type="button"
-        className="w-full text-white bg-primary mt-7"
-      >
+      <Button type="button" className="w-full text-white bg-primary mt-7">
         Save
       </Button>
     </div>
