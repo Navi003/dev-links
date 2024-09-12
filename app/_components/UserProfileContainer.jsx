82b10d77576ff5ab4a557f765938289af7ab7268
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-09-12 21:35:52 +0200

    added user form

--- app/_components/UserProfileContainer.jsx
+++ app/_components/UserProfileContainer.jsx
@@ -10,7 +10,32 @@ export default function UserProfileContainer() {
           Customize your Profile
         </Heading>
         <div>IMAGE UPLOAD</div>
-        <div>User Information</div>
+        <form className="p-6 rounded-lg bg-bg-light">
+          <div className="flex items-center justify-between mb-2">
+            <label className="flex-1">First Name</label>
+            <input
+              type="text"
+              placeholder="e.g John"
+              className="flex-1 px-5 py-3 text-lg rounded-md "
+            />
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <label className="flex-1">Last Name</label>
+            <input
+              type="text"
+              placeholder="e.g John"
+              className="flex-1 px-5 py-3 text-lg rounded-md "
+            />
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <label className="flex-1">E-Mail</label>
+            <input
+              type="email"
+              placeholder="e.g John"
+              className="flex-1 px-5 py-3 text-lg rounded-md "
+            />
+          </div>
+        </form>
       </div>
 
       <Button type="button" className="w-full mt-auto text-white bg-primary">
