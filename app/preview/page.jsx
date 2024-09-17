a11d677d048c242a1ad5ca3059b2458bba520498
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-09-17 22:21:07 +0200

    added Preview page

--- app/preview/page.jsx
+++ app/preview/page.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Button from "../_components/Button";
+import Link from "next/link";
 
 export default function page() {
-  return <div>Preview</div>;
+  return (
+    <>
+      <div className="absolute top-0 left-0 z-10 w-full min-h-[150vh] bg-black overflow-hidden">
+        <div className="flex">
+          <Link
+            className="absolute
+          right-5 top-5
+
+          px-5 py-3 border-[1px] 
+          
+          rounded-lg border-primary broder text-primary font-bold bg-secondary
+
+          
+          "
+            href="/"
+            type="link"
+          >
+            back
+          </Link>
+        </div>
+        <div className="flex items-center justify-center rounded-lg bg-primary">
+          <div className="bg-white w-80 h-96 translate-y-36"></div>
+        </div>
+      </div>
+    </>
+  );
 }
