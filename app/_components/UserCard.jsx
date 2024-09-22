963eb27bb1341c05be7dec3c7d937ed4fd9be6c0
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-09-22 22:28:31 +0200

    added useData State.

--- app/_components/UserCard.jsx
+++ app/_components/UserCard.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import { useLinks } from "./useLinkContext";
 
 export default function UserCard() {
-  const { imageSrc, url, setUrl, links, setLinks, user, setUser } = useLinks();
+  const { imageSrc, user, setUser, userData } = useLinks();
 
   return (
     <div className="flex flex-col items-center gap-5 p-10 bg-white rounded-lg shadow-lg translate-y-36 min-w-96 shadow-black ">
       <div className="w-32 h-32 overflow-hidden bg-red-500 rounded-full">
-        {imageSrc && (
+        {userData?.user?.image && (
           <Image
             className="object-cover w-full"
             src={imageSrc}
@@ -20,24 +20,19 @@ export default function UserCard() {
       </div>
       <div>
         <h2 className="text-3xl font-bold text-center">
-          {user.firstName} {user.lastName}
+          {userData.user?.firstName} {userData.user?.lastName}
         </h2>
         <p className="font-thin text-center">{user.email}</p>
       </div>
 
       {/* Make the parent ul full width */}
       <ul className="w-full">
-        {/* Make this li take full width */}
-        <li className="w-full p-2 text-center border rounded-md border-s">
-          Youtube
-        </li>
-
         {/* Loop through the links */}
-        {links.map((link) => {
+        {userData?.links?.map((link) => {
           return (
             <li
               key={link.platform}
-              className="w-full p-2 border rounded-md border-s"
+              className="w-full p-2 text-center border rounded-md border-s"
             >
               <a
                 target="_blank"
